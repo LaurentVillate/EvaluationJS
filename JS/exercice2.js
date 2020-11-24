a8fa4a3a291b6ac1acb5959081f92c38fd25007b
LaurentVillate <laurent.villate@yahoo.com>
2020-11-24 16:55:27 +0100

    Evaluation JS, exercice 1 terminé et 2 à finir

---
+++ JS/exercice2.js
@@ -0,0 +1,12 @@
+var multiplicateur = window.prompt("Entrez un multiplicateur");
+
+function tableMultiplication (multiplicateur)
+{
+    for (i=0; i<=10; i++)
+    {
+        result = i*multiplicateur;
+    }
+    return 
+}
+
+document.write(result)
